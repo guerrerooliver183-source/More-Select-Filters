e8ea52495e86c999d035db49ee598cb886550d24
guerrerooliver183-source <guerrerooliver183@gmail.com>
2026-04-18 12:46:26 -0500

    Update SelectFilterPopup.cpp

--- src/SelectFilterPopup.cpp
+++ src/SelectFilterPopup.cpp
@@ -9,13 +9,24 @@ bool filtersEnabled = true;
 
 MoreSelectFiltersPopup* MoreSelectFiltersPopup::create() { 
     auto ret = new MoreSelectFiltersPopup();
-    if (ret->initAnchored(350.0f, 276.0f)) {
+    if (ret->init()) {
         ret->autorelease();
         return ret;
     }
     delete ret;
     return nullptr;
 }
+
+bool MoreSelectFiltersPopup::init() {
+    if (!Popup::init(350.0f, 276.0f)) {
+        return false;
+    }
+
+    this->setup();
+
+    return true;
+}
+
 
 bool MoreSelectFiltersPopup::setup() {
     this->setTitle("Select Filter");
@@ -47,7 +58,7 @@ bool MoreSelectFiltersPopup::setup() {
     disableFiltersToggle->setPosition(290.0f, 25.0f);
     disableFiltersToggle->setScale(0.75f);
     mainMenu->addChild(disableFiltersToggle);
-    
+
     auto filtersContainer = CCNode::create();
     filtersContainer->setPosition(0.0f, 0.0f);
     m_filtersContainer = filtersContainer;
@@ -68,7 +79,7 @@ bool MoreSelectFiltersPopup::setup() {
         input->setScale(0.75f);
         menu->addChild(input);
         input->setFilter("1234567890abcdefghijklmnopqrstuvwxyz.,-");
-        auto clearButton = CCMenuItemSpriteExtra::create(CircleButtonSprite::create(CCSprite::create("clear.png"_spr), 
+        auto clearButton = CCMenuItemSpriteExtra::create(CircleButtonSprite::create(CCSprite::create("clear.png"_spr),
         CircleBaseColor::Gray), this, menu_selector(MoreSelectFiltersPopup::onClear));
         clearButton->setPosition(106.0f, 11.0f);
         clearButton->setScale(0.5f);
@@ -79,7 +90,7 @@ bool MoreSelectFiltersPopup::setup() {
         copyFromObjectButton->setScale(0.75f);
         copyFromObjectButton->m_baseScale = 0.75f;
         menu->addChild(copyFromObjectButton);
-        auto equalsToggler = CCMenuItemToggler::create(CircleButtonSprite::create(CCSprite::create("equals.png"_spr), CircleBaseColor::Cyan), 
+        auto equalsToggler = CCMenuItemToggler::create(CircleButtonSprite::create(CCSprite::create("equals.png"_spr), CircleBaseColor::Cyan),
         CircleButtonSprite::create(CCSprite::create("notequals.png"_spr), CircleBaseColor::DarkPurple), this, nullptr);
         equalsToggler->setPosition(106.0f, 35.0f);
         equalsToggler->setScale(0.5f);
@@ -110,7 +121,8 @@ bool MoreSelectFiltersPopup::setup() {
         }
         else string = filter.stringVal;
         auto input = getTextInput(filter.type);
-        input->setString(input->getString() + string + ",");
+        std::string currentStr = input->getString().c_str();
+        input->setString( (currentStr + string + ",").c_str() );
         getToggler(filter.type)->toggle(filter.notEquals);
     }
     for (auto menu : CCArrayExt<CCNode*>(filtersContainer->getChildren())) trimTextInput(menu->getID());
@@ -135,44 +147,56 @@ void MoreSelectFiltersPopup::onCopyFromObject(CCObject* sender) {
         return;
     }
     for (auto obj : CCArrayExt<GameObject*>(objs)) {
-        if (filter == "object id") input->setString(input->getString() + "," + std::to_string(obj->m_objectID));
+        std::string currentStr = input->getString().c_str();
+        
+        if (filter == "object id") input->setString( (currentStr + "," + std::to_string(obj->m_objectID)).c_str() );
         else if (filter == "group") {
             if (auto groups = obj->m_groups) {
                 for (auto group : *groups) {
-                    if (group != 0) input->setString(input->getString() + "," + std::to_string(group));
+                    if (group != 0) {
+                        currentStr = input->getString().c_str();
+                        input->setString( (currentStr + "," + std::to_string(group)).c_str() );
+                    }
                 }
             }
         }
         else if (filter == "layer") {
             auto layer = static_cast<int>(obj->m_zLayer) == 0 ? static_cast<int>(obj->m_defaultZLayer) : static_cast<int>(obj->m_zLayer);
-            input->setString(input->getString() + "," + intTypesMapReverse.at(layer));
+            input->setString( (currentStr + "," + intTypesMapReverse.at(layer)).c_str() );
         }
         else if (filter == "z order") {
             auto order = obj->m_zOrder == 0 ? obj->m_defaultZOrder : obj->m_zOrder;
-            input->setString(input->getString() + "," + std::to_string(order));
+            input->setString( (currentStr + "," + std::to_string(order)).c_str() );
         }
         else if (filter == "color") {
             std::vector<GJSpriteColor*> cols;
             for (auto col : {obj->m_baseColor, obj->m_detailColor}) if (col) cols.push_back(col);
             for (auto col : cols) {
                 auto color = col->m_colorID == 0 ? col->m_defaultColorID : col->m_colorID;
-                input->setString(input->getString() + "," + (intTypesMapReverse.contains(color) ? intTypesMapReverse.at(color) : std::to_string(color)));
+                currentStr = input->getString().c_str();
+                std::string val;
+                if (intTypesMapReverse.contains(color)) val = intTypesMapReverse.at(color);
+                else val = std::to_string(color);
+                input->setString( (currentStr + "," + val).c_str() );
             }
         }
         else if (filter == "scale") {
             std::string string = "";
             if (obj->m_scaleX == obj->m_scaleY) string = floatToFormattedString(obj->m_scaleX);
             else string = floatToFormattedString(obj->m_scaleX) + "x," + floatToFormattedString(obj->m_scaleY) + "y";
-            input->setString(input->getString() + "," + string);
+            input->setString( (currentStr + "," + string).c_str() );
         }
         else if (filter == "hsv") {
             std::vector<GJSpriteColor*> cols;
             for (auto col : {obj->m_baseColor, obj->m_detailColor}) if (col) cols.push_back(col);
             for (auto col : cols) {
                 if (!col->m_usesHSV) continue;
-                if (col->m_hsv.h != 0) input->setString(input->getString() + "," + floatToFormattedString(col->m_hsv.h) + "h");
-                if (col->m_hsv.s != 1) input->setString(input->getString() + "," + floatToFormattedString(col->m_hsv.s) + "s");
-                if (col->m_hsv.v != 1) input->setString(input->getString() + "," + floatToFormattedString(col->m_hsv.v) + "b");
+                currentStr = input->getString().c_str();
+                if (col->m_hsv.h != 0) input->setString( (currentStr + "," + floatToFormattedString(col->m_hsv.h) + "h").c_str() );
+                currentStr = input->getString().c_str();
+                if (col->m_hsv.s != 1) input->setString( (currentStr + "," + floatToFormattedString(col->m_hsv.s) + "s").c_str() );
+                currentStr = input->getString().c_str();
+                if (col->m_hsv.v != 1) input->setString( (currentStr + "," + floatToFormattedString(col->m_hsv.v) + "b").c_str() );
             }
         }
         else if (filter == "type") {
@@ -184,15 +208,16 @@ void MoreSelectFiltersPopup::onCopyFromObject(CCObject* sender) {
             if (obj->m_isHighDetail) string += ",detail";
             if (obj->m_objectType == GameObjectType::Hazard) string += ",hazard";
             if (obj->m_objectType == GameObjectType::Solid) string += ",solid";
-            input->setString(input->getString() + string);
+            input->setString( (currentStr + string).c_str() );
         }
         trimTextInput(filter);
-        input->setString(trimDuplicates(input->getString()));
+        std::string finalStr = input->getString().c_str();
+        input->setString( trimDuplicates(finalStr).c_str() );
     }
 }
 
 void MoreSelectFiltersPopup::onSaveValuesAndExit(CCObject* sender) {
-    saveValues(); // idk why i made this another function ngl, whatever
+    saveValues();
     this->onClose(nullptr);
 }
 
@@ -210,19 +235,22 @@ void MoreSelectFiltersPopup::onToggleFilters(CCObject* sender) {
     filtersEnabled = !filtersEnabled;
 }
 
-void MoreSelectFiltersPopup::saveValues() { // every filter obj has its own != thing cuz easier
+void MoreSelectFiltersPopup::saveValues() {
     currentFilter.clear();
     std::vector<std::string> type {"object id", "group", "layer", "z order", "color", "scale", "hsv", "type"};
     for (int i = 0; i < 5; i++) {
         auto notEquals = getTogglerEquals(type[i]);
-        for (auto num : getIntVector(getTextInput(type[i])->getString())) currentFilter.emplace_back(Filter{type[i], 0, num, {}, "", notEquals});
+        std::string str = getTextInput(type[i])->getString().c_str();
+        for (auto num : getIntVector(str)) currentFilter.emplace_back(Filter{type[i], 0, num, {}, "", notEquals});
     }
     for (int i = 0; i < 2; i++) {
         auto notEquals = getTogglerEquals(type[i + 5]);
-        for (auto& typedFloat : getTypedFloatVector(getTextInput(type[i + 5])->getString())) currentFilter.emplace_back(Filter{type[i + 5], 1, 0, typedFloat, "", notEquals});
+        std::string str = getTextInput(type[i + 5])->getString().c_str();
+        for (auto& typedFloat : getTypedFloatVector(str)) currentFilter.emplace_back(Filter{type[i + 5], 1, 0, typedFloat, "", notEquals});
     }
     auto notEquals = getTogglerEquals(type[7]);
-    for (auto& string : getStringVector(getTextInput(type[7])->getString())) currentFilter.emplace_back(Filter{type[7], 2, 0, {}, string, notEquals});
+    std::string str = getTextInput(type[7])->getString().c_str();
+    for (auto& string : getStringVector(str)) currentFilter.emplace_back(Filter{type[7], 2, 0, {}, string, notEquals});
 }
 
 TextInput* MoreSelectFiltersPopup::getTextInput(const std::string& id) {
@@ -235,12 +263,16 @@ TextInput* MoreSelectFiltersPopup::getTextInput(CCObject* button) {
 
 void MoreSelectFiltersPopup::trimTextInput(const std::string& id) {
     auto input = getTextInput(id);
-    auto string = input->getString();
-        if (!string.empty()) {
-            if (string.front() == ',') string.erase(string.begin());
-            if (string.back() == ',') string.pop_back();
+    std::string string = input->getString().c_str();
+    if (!string.empty()) {
+        if (string[0] == ',') {
+            string = string.substr(1);
         }
-        input->setString(string);
+        if (string[string.size() - 1] == ',') {
+            string = string.substr(0, string.size() - 1);
+        }
+    }
+    input->setString(string.c_str());
 }
 
 std::string MoreSelectFiltersPopup::getFilterType(CCObject* button) {
